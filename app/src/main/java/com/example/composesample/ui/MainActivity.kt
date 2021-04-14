454a39031ea50789c402c2e175551f99ec5337a0
Yuichi Ishibashi <redolphiner@gmail.com>
2021-04-15 02:01:30 +0900

    レイアウト変更

--- app/src/main/java/com/example/composesample/ui/MainActivity.kt
+++ app/src/main/java/com/example/composesample/ui/MainActivity.kt
@@ -3,15 +3,36 @@ package com.example.composesample.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import com.example.composesample.navigation.MainNavigator
 import com.example.composesample.ui.theme.ComposeSampleTheme
+import com.google.accompanist.systemuicontroller.LocalSystemUiController
+import com.google.accompanist.systemuicontroller.rememberAndroidSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeSampleTheme {
-                MainNavigator()
+            // Remember a controller, and provide it to the LocalSystemUiController
+            val controller = rememberAndroidSystemUiController()
+            CompositionLocalProvider(LocalSystemUiController provides controller) {
+                ComposeSampleTheme {
+                    // Get the current SystemUiController
+                    val systemUiController = LocalSystemUiController.current
+                    val useDarkIcons = MaterialTheme.colors.isLight
+                    SideEffect {
+                        // Update all of the system bar colors to be transparent, and use
+                        // dark icons if we're in light theme
+                        systemUiController.setSystemBarsColor(
+                            color = Color.Transparent,
+                            darkIcons = useDarkIcons
+                        )
+                    }
+                    MainNavigator()
+                }
             }
         }
     }
--- app/src/main/java/com/example/composesample/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/composesample/ui/screens/home/HomeScreen.kt
@@ -1,20 +1,66 @@
 package com.example.composesample.ui.screens.home
 
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.composesample.R
 import com.example.composesample.ui.theme.ComposeSampleTheme
 
 @Composable
 fun HomeScreen() {
     ComposeSampleTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background) {
-            Column {
-                Text("Hello World")
-            }
+        Column(modifier = Modifier.background(colorResource(id = R.color.navy))) {
+            TopAppBar(
+                title = {
+                    Text(text = "Compose Sample")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { }) {
+                        Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu Btn")
+                    }
+                },
+                backgroundColor = Color.Transparent,
+                contentColor = Color.White,
+                elevation = 2.dp
+            )
+            HomeContents()
+        }
+
+    }
+}
+
+@Composable
+fun HomeContents() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Home",
+            color = Color.White,
+            modifier = Modifier.padding(48.dp)
+        )
+        Button(
+            onClick = { /* Do something */ }
+        ) {
+            Text("Button")
         }
     }
 }
