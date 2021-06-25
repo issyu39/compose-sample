248981c1e3e35d19f147ab078ffd2a7f7b707965
Yuichi Ishibashi <issyu039@gmail.com>
2021-06-25 10:18:47 +0900

    Cardのリスト追加

--- app/src/main/java/com/example/composesample/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/composesample/ui/screens/home/HomeScreen.kt
@@ -1,8 +1,10 @@
 package com.example.composesample.ui.screens.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -10,7 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.composesample.R
 import com.example.composesample.ui.theme.ComposeSampleTheme
 
 @Composable
@@ -39,23 +44,109 @@ fun HomeTopBar() {
 
 @Composable
 fun HomeContents() {
-    Column(
+    LazyColumn(
         modifier = Modifier
             .background(MaterialTheme.colors.surface)
             .fillMaxWidth()
-            .fillMaxHeight(),
-        verticalArrangement = Arrangement.Center,
+            .fillMaxHeight()
+            .padding(start = 8.dp, end = 8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Home",
-            color = Color.White,
-            modifier = Modifier.padding(48.dp)
-        )
-        Button(
-            onClick = { /* Do something */ }
-        ) {
-            Text("Button")
+        item {
+            Card(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(240.dp)
+                    .padding(16.dp),
+            ) {
+                Image(
+                    painterResource(R.drawable.list_background_1),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                )
+            }
+        }
+        item {
+            Card(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(240.dp)
+                    .padding(16.dp),
+            ) {
+                Image(
+                    painterResource(R.drawable.list_background_2),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                )
+            }
+        }
+        item {
+            Card(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(240.dp)
+                    .padding(16.dp),
+            ) {
+                Image(
+                    painterResource(R.drawable.list_background_3),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                )
+            }
+        }
+        item {
+            Card(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(240.dp)
+                    .padding(16.dp),
+            ) {
+                Image(
+                    painterResource(R.drawable.list_background_4),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                )
+            }
+        }
+        item {
+            Card(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(240.dp)
+                    .padding(16.dp),
+            ) {
+                Image(
+                    painterResource(R.drawable.list_background_5),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                )
+            }
+        }
+        item {
+            Card(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(240.dp)
+                    .padding(16.dp),
+            ) {
+                Image(
+                    painterResource(R.drawable.list_background_6),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                )
+            }
         }
     }
 }
